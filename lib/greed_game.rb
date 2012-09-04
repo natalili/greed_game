bffdb250fcd5aafd9525f28528b105334d169cb8
natalili <natali.li@list.ru>
2012-09-04 16:00:55 +0300

    fixed code

--- lib/greed_game.rb
+++ lib/greed_game.rb
@@ -11,6 +11,7 @@ module GreedGame
   def initialize(*names)
     raise "It should be 2 or more players in the game" if names.size < 2
     @dices = 5
+    @max_score = 0
     @players = Array.new
     names.each do |name|
       @players << Player.new(name)
@@ -18,30 +19,38 @@ module GreedGame
   end
   
   def in_progress
-    max_score = 0
-    last_round = false
-    while  max_score < 3000 or not last_round
+    while  next_round?
+      calculate_max_score
       @players.each do |player|
         player.roll(@dices)
       end
-      player_whith_max_score = @players.max_by {|players| players.score_total }
-      last_round = true if max_score >= 3000
-      max_score = player_whith_max_score.score_total
     end
+    winner
+  end
+  
+  def finished
+    return "#{@winner[0].name} is winner" if @winner.count == 1
+    "Winner is absent"
+  end
+  
+  private
+  
+  def next_round?
+    @max_score < 3000 ? true : false
+  end
+  
+  def calculate_max_score
+    player_whith_max_score = @players.max_by {|players| players.score_total }
+    @max_score = player_whith_max_score.score_total
+  end
+  
+  def winner
+    calculate_max_score
     @winner = Array.new
     @players.each do |player|
-      @winner << player if player.score_total == max_score
+      @winner << player if player.score_total == @max_score
     end
     @winner
   end
   
-  def finished
-    if @winner.count == 1
-      text = "#{@winner[0].name} is winner"
-    else
-      text = "Winner is absent"
-    end
-    text
-  end
-
 end
